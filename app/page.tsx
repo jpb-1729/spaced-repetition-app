a4a14ac4b4a2ab964eca2f84afeba7b49840e7d1
John Bryan <bryanjp3.dev@gmail.com>
2025-11-03 10:52:39 -0500

    feat(ui): update flashcard spacing description to be more engaging

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <div className="flex flex-col">
       <header className="p-8">
-        <p className="text-4xl font-bold text-gray-900">Welcome to SR App</p>
+        <p className="text-4xl font-bold text-gray-900">Welcome to Olivero Recall</p>
+        <p className="text-xl text-gray-700"> Learn smarter, not harder. </p>
       </header>
       <div className="max-w-2xl p-8">
         <p className="leading-relaxed text-gray-700">
-          Reviewing a flashcard once is often insufficient to commit a fact to long-term memory, and
-          multiple reviews are necessary. This app handles spacing reviews for each flashcard to
-          commit facts to your long-term memory with minimal time invested in studying.
+          This app uses spaced repetition to burn knowledge into your memory with minimal effort.
         </p>
       </div>
     </div>
